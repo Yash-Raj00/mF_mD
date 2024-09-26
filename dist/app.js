4991aac110d9559a6791789ccdb1c1d073a6c753
Yash Raj Singh <010yash.forthe.code101@gmail.com>
2024-09-26 18:16:14 +0530

    same commit

--- dist/app.js
+++ dist/app.js
@@ -9,7 +9,7 @@ const cors_1 = __importDefault(require("cors"));
 const api_1 = require("./api");
 const app = (0, express_1.default)();
 // Middlewares
-app.use();
+app.use(cors());
 app.use(express_1.default.json());
 const port = process.env.APP_PORT || 3001;
 app.get("/", (req, res, next) => {
